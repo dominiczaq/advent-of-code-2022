450d69d7032be63b7505306d8a321b2ac11ba9a9
Grzegorz Dominiczak <grzegorz@tidio.net>
2022-12-08 22:58:35 +0100

    Day 1 part 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -8,23 +8,30 @@ fn main() {
         // .filter(|&x| !x.is_empty())
         .collect();
 
+    //part 1
     let mut max = 0;
-
     let mut local_sum = 0;
+    let mut elves_sums: Vec<i32> = vec![];
     for val in split {
         let is_empty = val.is_empty();
         if is_empty {
             if local_sum > max {
                 max = local_sum;
             }
+            elves_sums.push(local_sum);
             local_sum = 0;
         } else {
             let val: i32 = val.parse().unwrap_or(0);
             local_sum += val;
         }
     }
+    println!("Max: {:?}", max);
 
-    println!("{:?}", max);
+    //part 2
+    elves_sums.sort();
+    elves_sums.reverse();
+    let last_3 = elves_sums.get(0..3);
+    let sum_of_last_3: i32 = last_3.expect("Should have 3 top calories").iter().sum();
 
-    // println!("{:?}", split)
+    println!("Sum of last 3: {:?}", sum_of_last_3);
 }
